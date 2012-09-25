931f3acb89269b53e31d9629468136e7b63e555c
Martin Jackson <martin.jackson@bskyb.com>
2012-09-25 15:27:24 +0100

    - Added tomcat service

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,6 +27,7 @@ class tomcat {
   require tomcat::params
   include tomcat::repo
   include tomcat::packages
+  include tomcat::service
 
-  Class['tomcat::repo'] -> Class['tomcat::packages']
+  Class['tomcat::repo'] -> Class['tomcat::packages'] -> Class['tomcat::service']
 }

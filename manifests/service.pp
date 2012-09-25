24dbf7cabd0461b4145d6824aa665acb735464f9
Martin Jackson <martin.jackson@bskyb.com>
2012-09-25 16:27:01 +0100

    Fixing puppet-lint errors

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,9 +1,9 @@
 # tomcat::service
 class tomcat::service {
   service { 'tomcat7':
-    hasrestart => 'true',
-    hasstatus  => 'true',
-    enable     => 'true',
+    hasrestart => true,
+    hasstatus  => true,
+    enable     => true,
     ensure     => 'running',
   }
 }

57f4608d2751340e19b2edb9d735c4b545950be0
Martin Jackson <martin.jackson@bskyb.com>
2012-10-02 16:50:45 +0100

    That'll teach me for not using vagrant, fixing misspelt variable

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,8 +2,8 @@
 class tomcat::params {
   $jreyumrepopath = 'http://ip-10-227-94-135.eu-west-1.compute.internal/repo/'
   if $virtual == 'virtualbox' {
-    $eeyumrepoenabled = '0'
+    $jreyumrepoenabled = '0'
   } else {
-    $eeyumrepoenabled = '1'
+    $jreyumrepoenabled = '1'
   }
 }
